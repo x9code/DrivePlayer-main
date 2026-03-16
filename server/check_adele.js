0daca6137a24f8dfa423e22229f5dd6b8942b3de
x9code <kumarranad672@gmail.com>
2026-03-16 20:10:18 +0530

    chore: remove debug and one-time migration scripts

--- server/check_adele.js
+++
@@ -1,4 +0,0 @@
-const { pool } = require('./database/db');
-pool.query("SELECT name, mimeType FROM files WHERE name = 'Adele' LIMIT 1")
-    .then(res => { console.log(res.rows); process.exit(0); })
-    .catch(console.error);
